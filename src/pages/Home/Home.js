409ee7984e5b4ce6f74d9998bd147ec1d684b583
Valentin VERMANDE <vvermande@arketeam.com>
2022-10-03 17:18:17 +0200

    Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,59 @@
 import './Home.css'
+import Logo from '../../inktober_logo.png';
+import {useNavigate} from "react-router-dom";
 
 function Home(){
 
     const themes = [
         'gargoyle',
         'scurry',
-        'test',
-        'test',
-        'test',
-        'test',
-        'test',
-        'test',
-        'test',
-        'test',
+        'bat',
+        'scallop',
+        'flame',
+        'bouquet',
+        'trip',
+        'match',
+        'nest',
+        'crabby',
+        false,
+        'eagle',
+        'forget',
+        'kind',
+        'empty',
+        'armadillo',
+        'fowl',
+        'salty',
+        'scrape',
+        'ponytail',
+        'bluff',
+        false,
+        'bad dog',
+        'heist',
+        'booger',
+        'fairy',
+        'tempting',
+        'ego',
+        'snack',
+        'camping',
+        'uh-oh',
+        'gear',
+        'farm',
     ];
 
-    return <div className="grid">
-        {themes.map(theme =>
-            <div className="card">
-                {theme}
-            </div>
-        )}
+    const navigate = useNavigate();
+    let i = 1;
+    const day = 2;
+    themes.length = day;
+
+    return <div>
+        <img src={Logo} alt="Inktober2022"/>
+        <div className="grid">
+            {themes.map((theme) =>
+                <div className="card" onClick={() => navigate(theme)}>
+                    {theme && <>{i++}.{theme}</>}
+                </div>
+            )}
+        </div>
     </div>
 }
 
