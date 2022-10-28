afebbdbebbfddf01d50e96bed7a935af44b7b0dc
Valentin VERMANDE <vvermande@arketeam.com>
2022-10-28 14:02:20 +0200

    fix day27

--- src/pages/Week5/Day27/Day27.js
+++ src/pages/Week5/Day27/Day27.js
@@ -2,33 +2,31 @@ import './Day27.css';
 import {useEffect, useState} from "react";
 
 let last_update = new Date();
-let last_frame_update = new Date();
+let last_food_update = new Date();
 
 export const Day27 = () => {
 
     const [frame, setFrame] = useState(0);
-    const [food, setFood] = useState({type: 0, pos: 1});
+    const [food, setFood] = useState(1);
 
     const update = () => {
         const now = new Date();
         const dt = (now.getTime() - last_update.getTime())/1000;
-        last_update = now;
-        if((now.getTime() - last_frame_update.getTime())/1000 >= .05){
-            last_frame_update = now;
+        if(dt >= .05){
+            last_update = now;
             setFrame(f => (f + 1) % 3);
         }
-        setFood(f => {
-           f.pos -= .6 * dt;
-           if(f.pos <= 0){
-               f.pos = 1;
-               f.type = 1 + Math.floor(Math.random()*5);
-           }
-           return {...f};
-        });
+        const dt_food = (now.getTime() - last_food_update.getTime())/1000;
+        if(dt_food >= .8){
+            last_food_update = now;
+            setFood(1 + Math.floor(Math.random()*5))
+        }
+
+
     };
 
     useEffect(() => {
-        const interval = setInterval(update, 10);
+        const interval = setInterval(update, 5);
 
         return () => clearInterval(interval);
     }, []);
@@ -37,7 +35,7 @@ export const Day27 = () => {
         <div id="bg"></div>
         <div id="front"></div>
         <img src={`${process.env.PUBLIC_URL}/day27/${frame+1}.png`} alt="" id="kaaabi"/>
-        <img src={`${process.env.PUBLIC_URL}/day27/f${food.type}.png`} style={{'--pos': food.pos}} alt="" id="food"/>
+        <img src={`${process.env.PUBLIC_URL}/day27/f${food}.png`} alt="" id="food"/>
 
     </div>
 }
